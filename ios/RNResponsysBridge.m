83c120c2425bfbb87313e22ce7425c343537dc25
Marlon Constante <marlon.constante@gmail.com>
2020-10-02 23:55:03 -0300

    fix(ios): converted device token

--- ios/RNResponsysBridge.m
+++ ios/RNResponsysBridge.m
@@ -9,6 +9,26 @@
     return dispatch_get_main_queue();
 }
 
+- (NSData *)dataFromHexString:(NSString *)string
+{
+    string = [string lowercaseString];
+    NSMutableData *data= [NSMutableData new];
+    unsigned char whole_byte;
+    char byte_chars[3] = {'\0','\0','\0'};
+    int i = 0;
+    int length = string.length;
+    while (i < length-1) {
+        char c = [string characterAtIndex:i++];
+        if (c < '0' || (c > '9' && c < 'a') || c > 'f')
+            continue;
+        byte_chars[0] = c;
+        byte_chars[1] = [string characterAtIndex:i++];
+        whole_byte = strtol(byte_chars, NULL, 16);
+        [data appendBytes:&whole_byte length:1];
+    }
+    return data;
+}
+
 RCT_EXPORT_MODULE()
 
 + (void)didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
@@ -40,7 +60,7 @@ RCT_EXPORT_METHOD(trackEvent:(NSString *)eventName) {
 }
 
 RCT_EXPORT_METHOD(setDeviceToken:(NSString *)deviceToken) {
-    [[PushIOManager sharedInstance] didRegisterForRemoteNotificationsWithDeviceToken:[deviceToken dataUsingEncoding:NSUTF8StringEncoding]];
+    [[PushIOManager sharedInstance] didRegisterForRemoteNotificationsWithDeviceToken:[self dataFromHexString:deviceToken]];
 }
 
 RCT_EXPORT_METHOD(registerApp:(BOOL *)useLocation) {
